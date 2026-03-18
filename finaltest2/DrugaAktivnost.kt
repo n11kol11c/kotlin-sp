14e7f56874325c564397b7e4561bd1634621b461
м <174154109+n11kol11c@users.noreply.github.com>
2026-03-18 19:34:03 +0100

    Create DrugaAktivnost.kt

---
+++ finaltest2/DrugaAktivnost.kt
@@ -0,0 +1,35 @@
+package com.example.finaltest2
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DrugaAktivnost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_druga)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textView = findViewById<TextView>(R.id.textView2)
+        val ime = intent.getStringExtra("IME")
+
+        textView.text = "Zdravo, $ime"
+
+        val dugmeNazad = findViewById<Button>(R.id.buttonNazad)
+
+        dugmeNazad.setOnClickListener {
+            finish()
+        }
+    }
+}
